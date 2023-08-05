db7d12636e3e1225547fd9e547b740136b278ab7
Imrglop <69129770+Imrglop@users.noreply.github.com>
2023-08-05 10:39:20 -0400

    wow

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,9 @@ import typescript from "@rollup/plugin-typescript";
 export default {
   input: "./out/index.js",
   plugins: [match(), combine()],
+  onwarn: function (message) {
+    return;
+  }, // ignore all warnings. remove those 3 lines if you want warning output :)
   output: {
     file: "./dist/latiteapi.js",
     format: "es",
