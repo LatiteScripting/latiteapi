dd55c9635bdef741c50ab1f791949de3257952cc
Imrglop <69129770+Imrglop@users.noreply.github.com>
2024-01-19 17:57:07 -0500

    better naming

--- definitions/lib/filesystem.d.ts
+++ definitions/lib/filesystem.d.ts
@@ -5,7 +5,7 @@ declare namespace include {
          * @param path The path to read from.
          * @param callback 
          */
-        read(path: string, callback: (err: number, content: Buffer) => void): void;
+        readAsync(path: string, callback: (err: number, content: Buffer) => void): void;
         
         /**
          * 
@@ -13,30 +13,30 @@ declare namespace include {
          * @param data The data to write (see util.stringToBuffer to convert a string to a buffer)
          * @param callback 
          */
-        write(path: string, data: Buffer, callback: (err: number) => void): void;
+        writeAsync(path: string, data: Buffer, callback: (err: number) => void): void;
 
         /**
          * Reads a file from the path specified.
          * @param path The relative path from the script.
          */
-        readSync(path: string): Buffer;
+        read(path: string): Buffer;
         /**
          * Writes a file to the path specified.
          * @param path The relative path from the script.
          * @param data The data to write.
          */
-        writeSync(path: string, data: Buffer): void;
+        write(path: string, data: Buffer): void;
 
         /**
          * Checks if a file or a directory exists
          * @param path The path to check if a file or directory exists
          */
-        existsSync(path: string): boolean;
+        exists(path: string): boolean;
 
         /**
          * Creates a folder/directory.
          * @param path The path to create a directory.
          */
-        createDirectorySync(path: string): void;
+        createDirectory(path: string): void;
     }
 }
